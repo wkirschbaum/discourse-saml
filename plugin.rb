5fe11acd477d950321d3009996c996ab151f4bac
Blake Erickson <o.blakeerickson@gmail.com>
2017-03-13 08:12:48 -0600

    specify name ID format

--- plugin.rb
+++ plugin.rb
@@ -106,7 +106,7 @@ if request_method == 'post'
         settings.passive = false
         settings.issuer = Discourse.base_url
         settings.assertion_consumer_service_url = Discourse.base_url + "/auth/saml/callback"
-        settings.name_identifier_format = "urn:oasis:names:tc:SAML:2.0:protocol"
+        settings.name_identifier_format = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
 
         saml_params = authn_request.create_params(settings, {})
         @saml_req = saml_params['SAMLRequest']
